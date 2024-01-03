6d4df12848273cba0ce1716ae44f032abeea414f
tianxin-personal <96083098+tianxin-personal@users.noreply.github.com>
2024-01-03 14:44:08 -0600

    Completed the api

--- Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
+++ Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Infrastructure;
+﻿using Bookify.Api.Middleware;
+using Bookify.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Api.Extensions
@@ -10,6 +11,11 @@ namespace Bookify.Api.Extensions
             using var scope = app.ApplicationServices.CreateScope();
             using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             dbContext.Database.Migrate();
+        }
+
+        public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
         }
     }
 }
